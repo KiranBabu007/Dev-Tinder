0c98a47e5b2ef966b841facbe5a6788afccb2ffa
KiranBabu007 <kiranbabu3355@gmail.com>
2025-03-30 23:32:41 +0530

    add validator package and enhance user model validation; include email format check and skills field

--- src/models/user.js
+++ src/models/user.js
@@ -1,36 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+var validator = require('validator');
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true, // Ensure uniqueness at the database level
+      lowercase: true,
+      trim: true,
+      validate(value){
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email format");
+        }
+      }
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     age: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
+      min:18
     },
     gender: {
-        type: String,
-        required: true
-    }
-})
+      type: String,
+      required: true,
+    },
+    skills: {
+        type:[String]
+    },
 
-const User = mongoose.model('User', userSchema)
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = User
 
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
